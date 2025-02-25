2c4c44b75a37e5a5aa80c1b6076c66d7cd5d4021
Agabaa <133192595+Agabaa@users.noreply.github.com>
2025-02-25 19:49:13 +0300

    `Update index.html and js/change.js to reflect changes in button id from 'dark mode' to 'dark_mode'`

--- js/change.js
+++ js/change.js
@@ -1,3 +1,8 @@
-document.getElementById('dark mode').onclick= function(){
-    document.body.style.background='black';
+document.getElementById('dark_mode').onclick= function(){
+    document.body.style.background='rgb(55, 54, 54)';
+    document.body.style.color='rgb(223, 218, 218)';
+}
+document.getElementById('light_mode').onclick= function(){
+    document.body.style.background='white';
+    document.body.style.color='black';
 }
